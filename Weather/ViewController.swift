cb51e78ecface51b68de3f08817e28ba4c7f4722
Val Baca <valbaca@gmail.com>
2015-04-05 15:12:02 -0700

    Barebones setup. Pulls the url content and prints to console

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userCity: UITextField!
+    @IBOutlet var getWeatherButton: UIButton!
+
+    @IBOutlet var outputText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // for first try, just pulling info for london and extracting the weather information (not interacting with the input text or button).
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest")
+        
+        // check that url exists, before forcing it with `!`
+        if url != nil {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: {
+                (data, response, error) -> Void in
+                
+                var urlError = false
+                
+                if error == nil {
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    println(urlContent)
+                } else {
+                    urlError = true
+                }
+                
+                if urlError {
+                    self.showError()
+                }
+            })
+            task.resume()
+        } else {
+            showError()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func showError() {
+        outputText.text = "Was not able to find weather for \(userCity.text). Please try again"
+    }
 
+    @IBAction func getWeatherButtonPressed(sender: AnyObject) {
+    }
 }
 
